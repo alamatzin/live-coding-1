153b850c2edd5058f5e6581e23fefe6d3b10de5d
Alam Atzin <alamatzin@ciencias.unam.mx>
2022-09-29 21:50:19 -0500

    Ejercicio resuelto

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // Escribe tu código aquí.
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+const newArray = bigWords('bocina', myArray);
+let lista = document.getElementById("lista")
+
+console.log(newArray);
+printList(newArray)
+
+function bigWords(string, array){
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (element.length > string.length){
+            newArray.push(element)
+        }
+    }
+    return newArray;
+}
+
+function printList (array){
+    array.forEach(element => {
+        lista.innerHTML += `<li>${element}</li>`
+    });
+}
